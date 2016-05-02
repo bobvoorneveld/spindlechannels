67fdf9f5aa628f5db45896b61d541a3474dd8066
Bob Voorneveld <bob.voorneveld@wearespindle.com>
2016-05-02 10:11:24 +0200

    Make database user superuser so migrations will install extensions

--- Dockers/db/create_db_with_user.sql
+++ Dockers/db/create_db_with_user.sql
@@ -2,3 +2,4 @@ CREATE USER spindlechannels;
 CREATE DATABASE spindlechannels;
 GRANT ALL PRIVILEGES ON DATABASE spindlechannels TO spindlechannels;
 ALTER USER spindlechannels CREATEDB;
+ALTER USER spindlechannels WITH SUPERUSER;
