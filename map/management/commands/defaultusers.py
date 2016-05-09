f452cbbc6ec850eae44a06acf5a0248ecc4908fc
Bram Noordzij <bram@wearespindle.com>
2016-05-09 17:15:31 +0200

    default user pygrunn1/2

--- map/management/commands/defaultusers.py
+++ map/management/commands/defaultusers.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import IntegrityError
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = 'Add default demo users'
 
     def handle(self, *args, **options):
-		User.objects.create_user(username="green", password="green13May")
-		User.objects.create_user(username="blue", password="blue13May")
+		try:
+			User.objects.create_user(username="pygrunn1", password="13May")
+			User.objects.create_user(username="pygrunn2", password="13May")
+		except IntegrityError as ie:
+			pass
